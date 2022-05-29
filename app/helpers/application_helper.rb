38bf0a441c667db68ffead33f3e5a8de6d05ebb1
horimoto1 <ahs10261@gmail.com>
2022-05-29 17:26:54 +0900

    Adjust views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # 型に応じたパスを返す
+  def object_path(object)
+    case object
+    when Category
+      category_path(object)
+    when Article
+      category_article_path(object.category, object)
+    end
+  end
 end
