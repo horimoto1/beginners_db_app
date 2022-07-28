01e9a1377533309e078659d1a7810d578db43cdb
horimoto1 <ahs10261@gmail.com>
2022-07-28 18:20:06 +0900

    Adjust assets images

--- spec/features/errors/error_spec.rb
+++ spec/features/errors/error_spec.rb
@@ -2,27 +2,23 @@ require "rails_helper"
 
 RSpec.feature "Errors::Errors", type: :feature do
   feature "404エラーページのレイアウト" do
-    scenario "タイトルが正しいこと" do
+    background do
       visit "/undefined"
+    end
 
+    scenario "タイトルが正しいこと" do
       expect(page).to have_title "404 Not Found | DB入門"
     end
 
     scenario "見出し、画像が表示されること" do
-      visit "/undefined"
-
-      # 見出しが表示されること
       within "div.heading" do
         expect(page).to have_selector "h1", text: "404 Not Found"
       end
     end
 
     scenario "画像が表示されること" do
-      visit "/undefined"
-
-      # 画像が表示されること
       within "div.content" do
-        expect(find("img")[:src]).to match(%r{^/assets/404.*\.png$})
+        expect(find("img")[:src]).to match(%r{^/assets/404.*\.svg$})
       end
     end
   end
@@ -38,29 +34,23 @@ RSpec.feature "Errors::Errors", type: :feature do
       # モックでStandardErrorを発生させるようにする
       allow_any_instance_of(CategoriesController).to \
         receive(:show).and_raise(StandardError)
+
+      visit category_path(category)
     end
 
     scenario "タイトルが正しいこと" do
-      visit category_path(category)
-
       expect(page).to have_title "500 Server Error | DB入門"
     end
 
     scenario "見出しが表示されること" do
-      visit category_path(category)
-
-      # 見出しが表示されること
       within "div.heading" do
         expect(page).to have_selector "h1", text: "500 Server Error"
       end
     end
 
     scenario "画像が表示されること" do
-      visit category_path(category)
-
-      # 画像が表示されること
       within "div.content" do
-        expect(find("img")[:src]).to match(%r{^/assets/500.*\.png$})
+        expect(find("img")[:src]).to match(%r{^/assets/500.*\.svg$})
       end
     end
   end
@@ -68,18 +58,23 @@ RSpec.feature "Errors::Errors", type: :feature do
   feature "非公開ページのレイアウト" do
     given!(:article) { create(:article) }
 
-    scenario "タイトルが正しいこと" do
+    background do
       visit category_article_path(article.category, article)
+    end
 
+    scenario "タイトルが正しいこと" do
       expect(page).to have_title "非公開 | DB入門"
     end
 
     scenario "見出しが表示されること" do
-      visit category_article_path(article.category, article)
-
-      # 見出しが表示されること
       within "div.heading" do
         expect(page).to have_selector "h1", text: "非公開"
+      end
+    end
+
+    scenario "画像が表示されること" do
+      within "div.content" do
+        expect(find("img")[:src]).to match(%r{^/assets/NotPublished.*\.svg$})
       end
     end
   end
