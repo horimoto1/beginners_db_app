372305ed99c24f523a96fa7a468801c9df754db0
horimoto1 <ahs10261@gmail.com>
2022-09-11 20:58:15 +0900

    #45_Add_circleci_config

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
   gem "factory_bot_rails"
   gem "faker"
+  gem "rspec_junit_formatter"
   gem "rspec-rails", ">= 5.1.2", "< 6.0.0"
 end
 
---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /srv/beginners_db_app
+git pull
+
+docker-compose -f docker-compose.prod.yml run rails bundle install --without test development
+docker-compose -f docker-compose.prod.yml run rails yarn install
+docker-compose -f docker-compose.prod.yml run rails rails assets:precompile RAILS_ENV=production
