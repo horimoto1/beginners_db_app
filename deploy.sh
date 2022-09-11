362e4fbcfa591aaeb6699aa993a1a384b49c16a9
horimoto1 <ahs10261@gmail.com>
2022-09-12 01:29:53 +0900

    #45 Add deploy job

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ cd /srv/beginners_db_app
 git pull
 
 docker-compose -f docker-compose.prod.yml run rails bundle install --without test development
-docker-compose -f docker-compose.prod.yml run rails yarn install
+# docker-compose -f docker-compose.prod.yml run rails yarn install
 docker-compose -f docker-compose.prod.yml run rails rails assets:precompile RAILS_ENV=production
